38062754a3a53dc2863b799baa9987fad8344bf4
liyuan116 <1158084418@qq.com>
2017-09-06 11:08:29 +0800

    #update

---
+++ demo/ClangTest/ClangTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClangTest
+//
+//  Created by liyuan on 2017/9/4.
+//  Copyright © 2017年 liyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/ClangTest/ClangTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClangTest
+//
+//  Created by liyuan on 2017/9/4.
+//  Copyright © 2017年 liyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
